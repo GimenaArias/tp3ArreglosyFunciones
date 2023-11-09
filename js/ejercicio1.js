5e8a73b8ec698ad234503e992edbe608e7ed31bf
GimenaArias <gime.arias.az@gmail.com>
2023-11-09 14:49:15 -0300

    agrego el ejercicio 1

---
+++ js/ejercicio1.js
@@ -0,0 +1,5 @@
+document.write("<h1>Lista de meses:</h1>");
+var meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+for (var i = 0; i < meses.length; i++) {
+    document.write( "- " + meses[i] + "<br>");
+}
